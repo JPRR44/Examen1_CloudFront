bae390303039e1b97c60c7171f4970b7eba19946
JPRR44 <juanp.robles@hotmail.com>
2021-03-18 19:19:04 -0600

    Added axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const input = document.getElementById('text').value
+
+  const myFunction = (e) => {
+    e.preventDefault()
+    console.log(input)
+  }
+
   return (
     <div className="App">
-      Hello
+      <form>
+        <input placeholder='Add the text' id='text' />
+        <button onClick={myFunction}>Submit</button>
+      </form>
     </div>
   );
 }
